679579e595202b4c88104063a20e912c92bd1968
Thiago Carvalho <thiagocsousa@gmail.com>
2022-04-07 10:07:55 -0300

    Exercicio 12 com Asserts

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -15,26 +15,35 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
         cy.fixture('perfil').then(dados => {
-            cy.login(dados.usuario, dados.senha)     
+            cy.login(dados.usuario, dados.senha)
         })
-       
+
     });
 
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        
+
         ProdutoPage.adicionarProduto('Aero Daily Fitness Tee', 'XS', 'Black', 3)
+        cy.get('.woocommerce-message').should('contain', 'no seu carrinho.')
+
         ProdutoPage.adicionarProduto('Aether Gym Pant', '32', 'Blue', 1)
+        cy.get('.woocommerce-message').should('contain', 'no seu carrinho.')
+
         ProdutoPage.adicionarProduto('Ajax Full-Zip Sweatshirt', 'XS', 'Blue', 2)
+        cy.get('.woocommerce-message').should('contain', 'no seu carrinho.')
+
         ProdutoPage.adicionarProduto('Apollo Running Short', '32', 'Black', 2)
+        cy.get('.woocommerce-message').should('contain', 'no seu carrinho.')
+
 
         cy.get('#cart > .dropdown-toggle').click()
         cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
 
         FaturamentoPage.preencherFaturamento('Thiago', 'Sousa', 'Brasil', 'Rua Piauí', '100', 'Teresina', 'Piauí', '64063510', '86999999999', 'thiago@teste.com')
-    
         cy.get('#terms').check()
         cy.get('#place_order').click()
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+
 
     });
 
